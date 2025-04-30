6a54f2d34b08ace3399575985cff099c0ce01488
RahulSuroliya <suroliarahul9694@gmail.com>
2025-04-30 15:23:02 +0530

    ok

--- sop_django/ORSAPI/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from ORSAPI import views
-
-urlpatterns = [
-    path('<page>/<action>',views.action),
-    path('<page>/<action>/',views.action),
-    path('<page>/<action>/<int:id>',views.action),
-]
